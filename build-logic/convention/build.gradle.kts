966a2ca0ddcec98193d9a71351fc9f81ec232d97
aurora32s <aurora32s@naver.com>
2023-02-23 00:56:35 +0900

    [FEAT] #21 - AndroidApplicationConventionPlugin 추가

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    `kotlin-dsl`
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+}
+
+gradlePlugin {
+    plugins {
+        register("androidApplication") {
+            id = "daangn.android.application"
+            implementationClass = "AndroidApplicationConventionPlugin"
+        }
+    }
+}
