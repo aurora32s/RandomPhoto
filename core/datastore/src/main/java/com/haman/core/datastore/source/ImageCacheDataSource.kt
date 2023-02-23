ce289ab0b0295b1451cfc80b57f1a6e549686cb0
aurora32s <aurora32s@naver.com>
2023-02-23 21:38:42 +0900

    [FEAT] #8 - 이미지 메모리 캐싱 DataSource 구현

---
+++ core/datastore/src/main/java/com/haman/core/datastore/source/ImageCacheDataSource.kt
@@ -0,0 +1,18 @@
+package com.haman.core.datastore.source
+
+import android.graphics.Bitmap
+
+/**
+ * Image In Memory Cache
+ */
+interface ImageCacheDataSource {
+    /**
+     * id 에 해당하는 이미지 요청
+     */
+    fun getImage(id: String): Bitmap?
+
+    /**
+     * id에 해당하는 이미지 저장
+     */
+    fun addImage(id: String, bitmap: Bitmap): Pair<String, Bitmap>?
+}
