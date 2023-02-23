b29911fed68c5e77c4df46cb3a45b7b9b2fd6f12
aurora32s <aurora32s@naver.com>
2023-02-23 11:20:31 +0900

    [CREATE] #6 - create core.common module - android / hilt plugin

---
+++ core/common/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id ("daangn.android.application")
+    id("daangn.android.hilt")
+}
+
+android {
+    namespace = "com.haman.core.common"
+}
+
+dependencies {
+    testImplementation("junit:junit:4.13.2")
+}
