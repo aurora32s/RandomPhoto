7c82bb162520f9b7ccbc6a39a1a90d41427d6a68
aurora32s <aurora32s@naver.com>
2023-02-23 13:22:48 +0900

    [ADD] #6 - compose와 관련된 dependency를 version catalog로 이동

--- build-logic/convention/src/main/kotlin/AndroidApplicationComposeConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationComposeConventionPlugin.kt
@@ -2,6 +2,8 @@ import com.android.build.api.dsl.ApplicationExtension
 import ext.configureAndroidCompose
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
 
 class AndroidApplicationComposeConventionPlugin : Plugin<Project> {
@@ -10,6 +12,11 @@ class AndroidApplicationComposeConventionPlugin : Plugin<Project> {
             pluginManager.apply("com.android.application")
             val extension = extensions.getByType<ApplicationExtension>()
             configureAndroidCompose(extension)
+
+            val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+            dependencies {
+                "implementation"(libs.findBundle("androidx-compose").get())
+            }
         }
     }
 }
