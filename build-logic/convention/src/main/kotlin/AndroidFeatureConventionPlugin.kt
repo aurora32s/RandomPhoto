121697b960f22eb6495ebe463c680e4150d46274
aurora32s <aurora32s@naver.com>
2023-02-23 14:13:29 +0900

    [ADD] #6 - Feature module에서 공통적으로 의존하는 module 추가

--- build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -1,5 +1,6 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
 
 class AndroidFeatureConventionPlugin: Plugin<Project> {
     override fun apply(target: Project) {
@@ -7,6 +8,13 @@ class AndroidFeatureConventionPlugin: Plugin<Project> {
             pluginManager.apply {
                 apply("daangn.android.library")
                 apply("daangn.android.hilt")
+            }
+
+            dependencies {
+                "implementation"(project(":core:domain"))
+                "implementation"(project(":core:model"))
+                "implementation"(project(":core:ui"))
+                "implementation"(project(":core:common"))
             }
         }
     }
