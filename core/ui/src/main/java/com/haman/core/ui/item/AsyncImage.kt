ec670a7ca63ee75b8e9ec5b91e71fcbf76007b4f
aurora32s <aurora32s@naver.com>
2023-02-23 20:00:59 +0900

    [FEAT] #7 - 서버로부터 이미지를 요청하는 Image Component 구현

--- core/ui/src/main/java/com/haman/core/ui/item/AsyncImage.kt
+++ core/ui/src/main/java/com/haman/core/ui/item/AsyncImage.kt
@@ -1,7 +1,40 @@
 package com.haman.core.ui.item
 
+import android.graphics.Bitmap
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.asImageBitmap
 
 @Composable
-fun AsyncImage() {
+fun AsyncImage(
+    id: String,
+    width: Int = 200,
+    height: Int = 300,
+    requestImage: (String, Int, Int) -> Bitmap?
+) {
+    val bitmap = produceState<LoadImageState>(initialValue = LoadImageState.Loading) {
+        val result = requestImage(id, width, height)
+        value = result?.let { LoadImageState.Success(it) } ?: LoadImageState.Error
+    }
+    val resource = remember {
+        derivedStateOf {
+            when (val result = bitmap.value) {
+                LoadImageState.Loading -> 1
+                is LoadImageState.Success -> result.bitmap.asImageBitmap()
+                LoadImageState.Error -> 3
+            }
+        }
+    }
+}
+
+sealed class LoadImageState {
+    object Loading : LoadImageState()
+
+    data class Success(
+        val bitmap: Bitmap
+    ) : LoadImageState()
+
+    object Error : LoadImageState()
 }
