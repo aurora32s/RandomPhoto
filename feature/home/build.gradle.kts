1c8d47522fe84af7261219e97d26aeed719779a0
aurora32s <aurora32s@naver.com>
2023-02-23 12:00:13 +0900

    [CREATE] #6 - create feature.home module - android / compose / hilt plugin

---
+++ feature/home/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id ("daangn.android.application")
+    id ("daangn.android.application.compose")
+    id("daangn.android.hilt")
+}
+
+android {
+    namespace = "com.haman.feature.home"
+}
