7f0cc2969303ad61abcac42c60446eb419543082
aurora32s <aurora32s@naver.com>
2023-02-25 01:50:52 +0900

    [BASE] #9 - theme package를 app에서 core.designsystem module로 이동

--- core/designsystem/src/main/java/com/haman/core/designsystem/theme/Shape.kt
+++ core/designsystem/src/main/java/com/haman/core/designsystem/theme/Shape.kt
@@ -1,4 +1,4 @@
-package com.haman.daangnphoto.ui.theme
+package com.haman.core.designsystem.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
