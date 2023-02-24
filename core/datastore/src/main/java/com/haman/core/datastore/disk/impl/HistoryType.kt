be81bf9754a98940b71f92a9399dfc46361e5aef
aurora32s <aurora32s@naver.com>
2023-02-24 21:19:33 +0900

    [DELETE] #8 - 필요없는 코드 및 파일 제거

--- core/datastore/src/main/java/com/haman/core/datastore/disk/impl/HistoryType.kt
+++
@@ -1,9 +0,0 @@
-package com.haman.core.datastore.disk.impl
-
-enum class HistoryType {
-    CLEAN, DIRTY, REMOVE, READ, NONE;
-
-    companion object {
-        fun getHistoryType(type: Int) = values().first { it.ordinal == type }
-    }
-}
