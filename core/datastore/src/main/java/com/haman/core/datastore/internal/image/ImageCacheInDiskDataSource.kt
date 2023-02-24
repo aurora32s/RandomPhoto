a0547bca9efc99151e8ef6fc0eabe5fe5b4234d8
aurora32s <aurora32s@naver.com>
2023-02-24 21:13:19 +0900

    [FEAT] #8 - 이미지 Disk Cache 코드 변경

--- core/datastore/src/main/java/com/haman/core/datastore/internal/image/ImageCacheInDiskDataSource.kt
+++ core/datastore/src/main/java/com/haman/core/datastore/internal/image/ImageCacheInDiskDataSource.kt
@@ -1,19 +1,24 @@
 package com.haman.core.datastore.internal.image
 
+import android.content.Context
 import android.graphics.Bitmap
-import com.haman.core.datastore.disk.DiskCache
+import com.haman.core.datastore.disk.impl.DiskLruCache
 import com.haman.core.datastore.source.ImageCacheDataSource
+import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 class ImageCacheInDiskDataSource @Inject constructor(
-    private val diskCache: DiskCache
+    @ApplicationContext
+    context: Context
 ) : ImageCacheDataSource {
 
+    private val diskLruCache = DiskLruCache.open(context.filesDir, 1024 * 1024 * 10)
+
     override suspend fun getImage(id: String): Bitmap? {
-        return diskCache.getBitmapFromDisk(id)
+        return diskLruCache.getBitmapFromDisk(id)
     }
 
     override suspend fun addImage(id: String, bitmap: Bitmap) {
-        diskCache.putBitmapInDisk(id, bitmap)
+        diskLruCache.putBitmapInDisk(id, bitmap)
     }
 }
