2697f8283f8ce34839e5535de4879bda09b43336
aurora32s <aurora32s@naver.com>
2023-02-24 21:54:24 +0900

    [FEAT] #8 - 캐싱 작업은 IO DIspatcher에서 수행되도록 코드 변경

--- core/domain/src/main/java/com/haman/core/domain/GetImageUseCase.kt
+++ core/domain/src/main/java/com/haman/core/domain/GetImageUseCase.kt
@@ -8,6 +8,6 @@ class GetImageUseCase @Inject constructor(
     private val imageRepository: ImageRepository
 ) {
     suspend operator fun invoke(id: String, width: Int, height: Int): Bitmap? {
-        return imageRepository.getImage(id, width, height)
+        return imageRepository.getImage(id, width, height).getOrNull()
     }
 }
