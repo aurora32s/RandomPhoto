5176fbc230182afba649c15aeb7da8f2a2cb6382
aurora32s <aurora32s@naver.com>
2023-02-25 01:15:21 +0900

    [FEAT] #8 - 이미지 Disk Cache 동기화 코드 및 주석 추가

--- core/datastore/src/main/java/com/haman/core/datastore/internal/image/ImageCacheInDiskDataSource.kt
+++ core/datastore/src/main/java/com/haman/core/datastore/internal/image/ImageCacheInDiskDataSource.kt
@@ -2,17 +2,27 @@ package com.haman.core.datastore.internal.image
 
 import android.content.Context
 import android.graphics.Bitmap
+import com.haman.core.common.di.IODispatcher
 import com.haman.core.datastore.disk.impl.DiskLruCache
 import com.haman.core.datastore.source.ImageCacheDataSource
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class ImageCacheInDiskDataSource @Inject constructor(
     @ApplicationContext
-    context: Context
+    context: Context,
+    @IODispatcher val ioDispatcher: CoroutineDispatcher
 ) : ImageCacheDataSource {
+    private lateinit var diskLruCache: DiskLruCache
 
-    private val diskLruCache = DiskLruCache.open(context.filesDir, 1024 * 1024 * 10)
+    init {
+        CoroutineScope(ioDispatcher).launch {
+            diskLruCache = DiskLruCache.open(context.filesDir, 1024 * 1024 * 10)
+        }
+    }
 
     override suspend fun getImage(id: String): Bitmap? {
         return diskLruCache.getBitmapFromDisk(id)
