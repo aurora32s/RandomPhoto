9b90d30e6c81e9e949566d49da41f30aaa4da639
aurora32s <aurora32s@naver.com>
2023-02-25 20:08:05 +0900

    [FEAT] #7 - 이미지 로딩/성공/실패 처리

--- core/designsystem/src/main/java/com/haman/core/designsystem/icon/DaangnIcons.kt
+++ core/designsystem/src/main/java/com/haman/core/designsystem/icon/DaangnIcons.kt
@@ -3,6 +3,7 @@ package com.haman.core.designsystem.icon
 import com.haman.core.designsystem.R
 
 object DaangnIcons {
+    val logo = R.drawable.logo
     val close = R.drawable.close
     val linearBtn = R.drawable.linear_btn
     val gridBtn = R.drawable.grid_btn
