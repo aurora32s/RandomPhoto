9fc230ec896d20e41974d6e7a55d76b066779185
aurora32s <aurora32s@naver.com>
2023-02-25 18:17:22 +0900

    [FEAT] #16 - DetailViewModel에 이미지 요청 UseCase 및 메서드 추가

--- feature/detail/src/main/java/com/haman/feature/detail/DetailScreen.kt
+++ feature/detail/src/main/java/com/haman/feature/detail/DetailScreen.kt
@@ -1,7 +1,13 @@
 package com.haman.feature.detail
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.haman.core.designsystem.component.AsyncImage
 
 @Composable
-fun DetailScreen() {
+fun DetailScreen(
+    imageId: String,
+    viewModel: DetailViewModel = hiltViewModel()
+) {
+    AsyncImage(id = imageId, load = viewModel::getImageByUrl)
 }
