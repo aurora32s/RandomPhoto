35be4f40dd43ac263a090cf44d256f23cd4b2b6d
aurora32s <aurora32s@naver.com>
2023-02-25 20:25:53 +0900

    [UI] #16 - DetailScreen 기본 UI 코드 추가

--- core/designsystem/src/main/java/com/haman/core/designsystem/component/AsyncImage.kt
+++ core/designsystem/src/main/java/com/haman/core/designsystem/component/AsyncImage.kt
@@ -5,14 +5,14 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.produceState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import com.haman.core.designsystem.icon.DaangnIcons
 import com.haman.core.designsystem.theme.Black
@@ -47,8 +47,10 @@ fun AsyncImage(
     ) {
         when (val result = bitmap.value) {
             is LoadImageState.Success -> Image(
+                modifier = Modifier.fillMaxWidth(),
                 bitmap = result.bitmap.asImageBitmap(),
-                contentDescription = "Image's id is $id"
+                contentDescription = "Image's id is $id",
+                contentScale = ContentScale.FillWidth
             )
             is LoadImageState.Error -> Image(
                 painter = painterResource(id = DaangnIcons.logo),
