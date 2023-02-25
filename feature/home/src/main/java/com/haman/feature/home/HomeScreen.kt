bfc20e21b31c49215fe2a56fb0bae6a247040547
aurora32s <aurora32s@naver.com>
2023-02-26 01:12:46 +0900

    [FEAT] #12 - list type에 따른 리스트 변환 코드 정리 및 list type에 따른 item UI 세부사항 추가(ex. contentPadding 등)

--- feature/home/src/main/java/com/haman/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/com/haman/feature/home/HomeScreen.kt
@@ -1,22 +1,21 @@
 package com.haman.feature.home
 
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Scaffold
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
-import androidx.paging.compose.items
 import com.haman.core.designsystem.component.AsyncImage
 import com.haman.core.designsystem.icon.DaangnIcons
 import com.haman.core.ui.item.ImageLinearItem
 import com.haman.core.ui.list.GridPagingList
 import com.haman.core.ui.list.PagingList
-import com.haman.core.ui.list.VerticalPagingList
 
 @Composable
 fun HomeScreen(
@@ -24,11 +23,17 @@ fun HomeScreen(
 ) {
     val images = viewModel.imagesInfo.collectAsLazyPagingItems()
     val listType = viewModel.listType.collectAsState()
+    val listState = rememberLazyListState()
 
     Scaffold(
         floatingActionButton = {
-            FloatingActionButton(onClick = { viewModel.toggleListType() }) {
+            FloatingActionButton(
+                backgroundColor = MaterialTheme.colors.primary,
+                contentColor = MaterialTheme.colors.onPrimary,
+                onClick = { viewModel.toggleListType() }
+            ) {
                 Icon(
+                    modifier = Modifier.size(20.dp),
                     painter = painterResource(
                         id = when (listType.value) {
                             ListType.LINEAR -> DaangnIcons.gridBtn
@@ -41,16 +46,24 @@ fun HomeScreen(
         }
     ) {
         PagingList(data = images) {
-            when (listType.value) {
-                ListType.LINEAR -> VerticalPagingList(data = images) {
-                    ImageLinearItem(
+            GridPagingList(
+                cell = listType.value.column,
+                contentPadding = 8f,
+                data = images,
+                listState = listState
+            ) {
+                when (listType.value) {
+                    ListType.GRID -> AsyncImage(
+                        modifier = Modifier
+                            .aspectRatio(1f),
+                        id = it.id,
+                        load = viewModel::getImageByUrl
+                    )
+                    ListType.LINEAR -> ImageLinearItem(
                         imageId = it.id,
                         author = it.author,
                         loadImage = viewModel::getImageByUrl
                     )
-                }
-                ListType.GRID -> GridPagingList(data = images) {
-                    AsyncImage(id = it.id, load = viewModel::getImageByUrl)
                 }
             }
         }
