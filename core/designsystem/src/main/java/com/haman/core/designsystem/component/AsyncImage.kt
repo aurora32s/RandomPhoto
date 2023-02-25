009c30ec1168bc6670efe6696acd929892ff943d
aurora32s <aurora32s@naver.com>
2023-02-25 20:08:05 +0900

    [FEAT] #7 - 이미지 로딩/성공/실패 처리

--- core/designsystem/src/main/java/com/haman/core/designsystem/component/AsyncImage.kt
+++ core/designsystem/src/main/java/com/haman/core/designsystem/component/AsyncImage.kt
@@ -2,28 +2,60 @@ package com.haman.core.designsystem.component
 
 import android.graphics.Bitmap
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.res.painterResource
+import com.haman.core.designsystem.icon.DaangnIcons
+import com.haman.core.designsystem.theme.Black
+import com.haman.core.designsystem.theme.Gray200
+import com.haman.core.designsystem.theme.Gray700
+import com.haman.core.designsystem.theme.Gray900
 
 @Composable
 fun AsyncImage(
+    modifier: Modifier = Modifier,
     width: Int = 200,
     height: Int = 300,
     id: String,
-    load: suspend (String, Int, Int) -> Bitmap?
+    load: suspend (String, Int, Int) -> Bitmap?,
+    isDarkTheme: Boolean = isSystemInDarkTheme()
 ) {
     val bitmap = produceState<LoadImageState>(initialValue = LoadImageState.Loading) {
         val result = load(id, width, height)
         value = result?.let { LoadImageState.Success(it) } ?: LoadImageState.Error
     }
+    val backgroundColor = remember {
+        derivedStateOf {
+            when (bitmap.value) {
+                LoadImageState.Error -> if (isDarkTheme) Black else Gray900
+                else -> if (isDarkTheme) Gray700 else Gray200
+            }
+        }
+    }
 
-    when (val result = bitmap.value) {
-        is LoadImageState.Success -> Image(
-            bitmap = result.bitmap.asImageBitmap(),
-            contentDescription = ""
-        )
-        else -> {}
+    Box(
+        modifier = modifier.background(backgroundColor.value)
+    ) {
+        when (val result = bitmap.value) {
+            is LoadImageState.Success -> Image(
+                bitmap = result.bitmap.asImageBitmap(),
+                contentDescription = "Image's id is $id"
+            )
+            is LoadImageState.Error -> Image(
+                painter = painterResource(id = DaangnIcons.logo),
+                contentDescription = "Fail to load $id Image"
+            )
+            else -> {}
+        }
     }
 }
 
--- core/designsystem/src/main/java/com/haman/core/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/com/haman/core/designsystem/theme/Color.kt
@@ -5,5 +5,7 @@ import androidx.compose.ui.graphics.Color
 val Orange800 = Color(0xFFEF6C00)
 val White = Color(0xFFFFFFFF)
 val Black = Color(0xFF000000)
-val Gray600 = Color(0xFF757575)
 val Gray900 = Color(0xFF212121)
+val Gray700 = Color(0xFF616161)
+val Gray600 = Color(0xFF757575)
+val Gray200 = Color(0xFFEEEEEE)
