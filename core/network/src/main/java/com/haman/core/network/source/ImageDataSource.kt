8345edcdb0f73bd0d08ee5f327942a28e16965bc
aurora32s <aurora32s@naver.com>
2023-02-25 13:37:37 +0900

    [FEAT] #14 - 특정 이미지의 정보를 요청하는 메서드를 ImageDataSource에 추가

--- core/network/src/main/java/com/haman/core/network/source/ImageDataSource.kt
+++ core/network/src/main/java/com/haman/core/network/source/ImageDataSource.kt
@@ -1,6 +1,7 @@
 package com.haman.core.network.source
 
 import android.graphics.Bitmap
+import com.haman.core.network.response.ImageResponse
 
 interface ImageDataSource {
     /**
@@ -10,4 +11,10 @@ interface ImageDataSource {
      * @param height 이미지 세로 길이
      */
     suspend fun getImage(id: String, width: Int, height: Int): Result<Bitmap>
+
+    /**
+     * 특정 이미지의 정보 요청
+     * @param id 이미지 id
+     */
+    suspend fun getImageInfo(id: String): Result<ImageResponse>
 }
