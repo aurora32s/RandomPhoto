42297e48f5f5e8499b0d98b5ff88c4f86c47c4a6
aurora32s <aurora32s@naver.com>
2023-02-25 13:48:05 +0900

    [FEAT] #14 - 특정 이미지의 정보를 요청하는 UseCase 추가(core.domain)

---
+++ core/domain/src/main/java/com/haman/core/domain/GetImageInfoUseCase.kt
@@ -0,0 +1,18 @@
+package com.haman.core.domain
+
+import com.haman.core.data.repository.ImageRepository
+import com.haman.core.domain.model.Image
+import com.haman.core.domain.model.toModel
+import javax.inject.Inject
+
+/**
+ * 특정 이미지의 정보를 요청하는 Use Case
+ */
+class GetImageInfoUseCase @Inject constructor(
+    private val imageRepository: ImageRepository
+) {
+    suspend operator fun invoke(id: String): Image? {
+        val image = imageRepository.getImageInfo(id)
+        return if (image.isSuccess) image.getOrNull()?.toModel() else null
+    }
+}
