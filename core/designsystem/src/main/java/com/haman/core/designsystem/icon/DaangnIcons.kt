56960204f561a4c76606c68e3f245c48b04d5d1c
aurora32s <aurora32s@naver.com>
2023-02-25 15:53:20 +0900

    [BASE] #33 - Icon 을 쉽게 가져다 사용할 수 있도록 object class 로 관리하도록 구현

---
+++ core/designsystem/src/main/java/com/haman/core/designsystem/icon/DaangnIcons.kt
@@ -0,0 +1,10 @@
+package com.haman.core.designsystem.icon
+
+import com.haman.core.designsystem.R
+
+object DaangnIcons {
+    val close = R.drawable.close
+    val linearBtn = R.drawable.linear_btn
+    val gridBtn = R.drawable.grid_btn
+    val refresh = R.drawable.grid_btn
+}
