63ed5fcbe0cca8e45a0e9efab15c3747eb425074
aurora32s <aurora32s@naver.com>
2023-02-26 01:12:46 +0900

    [FEAT] #12 - list type에 따른 리스트 변환 코드 정리 및 list type에 따른 item UI 세부사항 추가(ex. contentPadding 등)

--- core/ui/src/main/java/com/haman/core/ui/list/GridPagingList.kt
+++ core/ui/src/main/java/com/haman/core/ui/list/GridPagingList.kt
@@ -1,23 +1,32 @@
 package com.haman.core.ui.list
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.lazy.GridCells
-import androidx.compose.foundation.lazy.LazyVerticalGrid
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.paging.compose.LazyPagingItems
 import com.haman.core.model.ui.UiModel
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun <T : UiModel> GridPagingList(
+    modifier: Modifier = Modifier,
     cell: Int = 2,
+    contentPadding: Float = 0f,
     data: LazyPagingItems<T>,
+    listState: LazyListState,
     item: @Composable (T) -> Unit
 ) {
     LazyVerticalGrid(
-        cells = GridCells.Fixed(cell)
+        modifier = modifier,
+        cells = GridCells.Fixed(cell),
+        state = listState,
+        contentPadding = PaddingValues(contentPadding.dp),
+        verticalArrangement = Arrangement.spacedBy(contentPadding.dp),
+        horizontalArrangement = Arrangement.spacedBy(contentPadding.dp)
     ) {
         items(data.itemCount) { index ->
             data[index]?.let { item(it) }
