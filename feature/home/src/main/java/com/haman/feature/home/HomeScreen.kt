dfb8032933fcb8e27ff45f941588dec0a6ec26e6
aurora32s <aurora32s@naver.com>
2023-02-25 16:00:04 +0900

    [REFACTOR] #33 - AsyncImage를 core.ui에서 core.designsystem으로 이동

--- feature/home/src/main/java/com/haman/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/com/haman/feature/home/HomeScreen.kt
@@ -2,11 +2,9 @@ package com.haman.feature.home
 
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.haman.core.ui.item.AsyncImage
 
 @Composable
 fun HomeScreen(
     viewModel: HomeViewModel = hiltViewModel()
 ) {
-    AsyncImage(id = "0", load = viewModel::getImageByUrl)
 }
