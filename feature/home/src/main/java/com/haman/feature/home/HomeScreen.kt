6d48e837e235144d5744fa18ee1490b24f7b0784
aurora32s <aurora32s@naver.com>
2023-02-25 13:21:33 +0900

    [FEAT] #11 - 이미지 리스트 및 특정 이미지 요청 로직 추가 ( * 추가 Dependency: hilt-navigation-compose )

--- feature/home/src/main/java/com/haman/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/com/haman/feature/home/HomeScreen.kt
@@ -1,7 +1,12 @@
 package com.haman.feature.home
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.haman.core.ui.item.AsyncImage
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    viewModel: HomeViewModel = hiltViewModel()
+) {
+    AsyncImage(id = "0", load = viewModel::getImageByUrl)
 }
--- feature/home/src/main/java/com/haman/feature/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/haman/feature/home/HomeViewModel.kt
@@ -1,12 +1,39 @@
 package com.haman.feature.home
 
+import android.graphics.Bitmap
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import androidx.paging.map
+import com.haman.core.domain.GetImageUseCase
 import com.haman.core.domain.GetImagesInfoUseCase
+import com.haman.feature.home.model.image.ImageUiModel
+import com.haman.feature.home.model.image.toUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val getImagesInfoUseCase: GetImagesInfoUseCase
+    getImagesInfoUseCase: GetImagesInfoUseCase,
+    private val getImageUseCase: GetImageUseCase
 ) : ViewModel() {
+
+    val imagesInfo: Flow<PagingData<ImageUiModel>> =
+        getImagesInfoUseCase()
+            .map { it.map { image -> image.toUiModel() } }
+            .cachedIn(viewModelScope)
+
+    /**
+     * 이미지 id 를 이용해
+     */
+    suspend fun getImageByUrl(id: String, width: Int, height: Int): Bitmap? {
+        return viewModelScope
+            .async { getImageUseCase(id, width, height) }
+            .await()
+    }
 }
