7d7bfcc77fa5c7265de8c0ebee13cfa02afcef02
aurora32s <aurora32s@naver.com>
2023-02-25 23:57:49 +0900

    [FEAT] #12 - list type에 따라 다른 형태의 리스트를 보여줄 수 있도록 수정

--- feature/home/src/main/java/com/haman/feature/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/haman/feature/home/HomeViewModel.kt
@@ -11,9 +11,8 @@ import com.haman.core.domain.GetImagesInfoUseCase
 import com.haman.core.model.ui.ImageUiModel
 import com.haman.core.model.ui.toUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.async
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 
 @HiltViewModel
@@ -21,6 +20,12 @@ class HomeViewModel @Inject constructor(
     getImagesInfoUseCase: GetImagesInfoUseCase,
     private val getImageUseCase: GetImageUseCase
 ) : ViewModel() {
+
+    private val _listType = MutableStateFlow<ListType>(ListType.LINEAR)
+    val listType: StateFlow<ListType>
+        get() = _listType.asStateFlow()
+
+    private var loadImageJob = SupervisorJob()
 
     val imagesInfo: Flow<PagingData<ImageUiModel>> =
         getImagesInfoUseCase()
@@ -31,8 +36,24 @@ class HomeViewModel @Inject constructor(
      * 이미지 id 를 이용해
      */
     suspend fun getImageByUrl(id: String, width: Int, height: Int): Bitmap? {
-        return viewModelScope
-            .async { getImageUseCase(id, width, height) }
-            .await()
+        return viewModelScope.async {
+            getImageByUrl(id, width, height)
+        }.await()
     }
+
+    fun toggleListType() {
+        viewModelScope.launch {
+            _listType.emit(
+                when (_listType.value) {
+                    ListType.LINEAR -> ListType.GRID
+                    ListType.GRID -> ListType.LINEAR
+                }
+            )
+        }
+    }
+}
+
+enum class ListType(val column: Int) {
+    LINEAR(1),
+    GRID(3)
 }
