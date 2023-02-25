85255b17ee1a4f219a3b57a5cfc555ebab58c115
aurora32s <aurora32s@naver.com>
2023-02-25 20:25:53 +0900

    [UI] #16 - DetailScreen 기본 UI 코드 추가

--- feature/detail/src/main/java/com/haman/feature/detail/DetailScreen.kt
+++ feature/detail/src/main/java/com/haman/feature/detail/DetailScreen.kt
@@ -1,12 +1,14 @@
 package com.haman.feature.detail
 
-import android.graphics.Bitmap
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.haman.core.designsystem.component.AsyncImage
-import com.haman.core.model.ui.ImageUiModel
+import com.haman.core.designsystem.component.SubTitle
 
 @Composable
 fun DetailScreen(
@@ -17,22 +19,27 @@ fun DetailScreen(
         viewModel.getImageInfo(imageId = imageId)
     }
     val detailUiState = viewModel.detailUiState.collectAsState()
-    when (val value = detailUiState.value) {
-        DetailUiState.Error -> {}
-        DetailUiState.Loading -> {}
-        is DetailUiState.Success -> {
-            DetailBody(
-                image = value.image,
-                loadImage = viewModel::getImageByUrl
-            )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(vertical = 24.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        when (val value = detailUiState.value) {
+            DetailUiState.Error -> {}
+            DetailUiState.Loading -> {}
+            is DetailUiState.Success -> {
+                SubTitle(
+                    modifier = Modifier.padding(4.dp),
+                    text = "by ${value.image.author}"
+                )
+                AsyncImage(
+                    modifier = Modifier.fillMaxWidth(),
+                    id = value.image.id,
+                    load = viewModel::getImageByUrl
+                )
+            }
         }
     }
-}
-
-@Composable
-fun DetailBody(
-    image: ImageUiModel,
-    loadImage: suspend (String, Int, Int) -> Bitmap?
-) {
-    AsyncImage(id = image.id, load = loadImage)
 }
