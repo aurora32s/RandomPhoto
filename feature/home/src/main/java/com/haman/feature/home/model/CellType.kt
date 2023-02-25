20f48d192a7b67e01df5872f9412317ee8cfac01
aurora32s <aurora32s@naver.com>
2023-02-25 13:21:33 +0900

    [FEAT] #11 - 이미지 리스트 및 특정 이미지 요청 로직 추가 ( * 추가 Dependency: hilt-navigation-compose )

---
+++ feature/home/src/main/java/com/haman/feature/home/model/CellType.kt
@@ -0,0 +1,9 @@
+package com.haman.feature.home.model
+
+/**
+ * feature.home 에서 사용하는 각 element 타입
+ */
+enum class CellType {
+    GridImage, // GridLayout 에 사용되는 Item
+    LinearImage // LinearImage 에 사용되는 Item
+}
