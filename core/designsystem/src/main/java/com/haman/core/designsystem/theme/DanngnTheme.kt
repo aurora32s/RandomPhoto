f6ac8bdba723a452f5fc6b89acdf680f79c45efa
aurora32s <aurora32s@naver.com>
2023-02-26 19:03:15 +0900

    [FEAT] #12 - windowstatusbar 색상 변경

--- core/designsystem/src/main/java/com/haman/core/designsystem/theme/DanngnTheme.kt
+++ core/designsystem/src/main/java/com/haman/core/designsystem/theme/DanngnTheme.kt
@@ -1,10 +1,16 @@
 package com.haman.core.designsystem.theme
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorPalette = darkColors(
     primary = Orange800,
@@ -33,6 +39,15 @@ fun DaangnPhotoTheme(
         DarkColorPalette
     } else {
         LightColorPalette
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colors.background.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme.not()
+        }
     }
 
     MaterialTheme(
