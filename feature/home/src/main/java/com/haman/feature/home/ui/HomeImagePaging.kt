de67f45969fed2a807098acf9708ce4f8962b973
aurora32s <aurora32s@naver.com>
2023-02-27 00:01:11 +0900

    [FEAT] #13 추가적인 HomeScreen 코드 정리 및 이미지 리스트 로딩/성공/실패 UI 처리 추가

--- feature/home/src/main/java/com/haman/feature/home/ui/HomeImagePaging.kt
+++ feature/home/src/main/java/com/haman/feature/home/ui/HomeImagePaging.kt
@@ -7,73 +7,33 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
 import androidx.paging.compose.LazyPagingItems
-import com.haman.core.common.state.ToastPosition
-import com.haman.core.designsystem.R
 import com.haman.core.model.ui.ImageUiModel
-import com.haman.core.ui.list.PagingList
-import com.haman.core.ui.state.ToolbarState
 import com.haman.feature.home.ListType
-
-/**
- * 페이징 상태에 따른 UI 처리
- */
-@Composable
-fun HomeImagePaging(
-    modifier: Modifier = Modifier,
-    images: LazyPagingItems<ImageUiModel>,
-    toolbarState: ToolbarState,
-    listState: LazyListState = rememberLazyListState(),
-    listType: ListType,
-    toast: (ToastPosition, Int) -> Unit,
-    toDetail: (String) -> Unit,
-    loadImage: suspend (String, Int, Int) -> Bitmap?
-) {
-    PagingList(
-        modifier = modifier
-            .graphicsLayer {
-                translationY = toolbarState.height
-            },
-        data = images,
-        errorMsg = R.string.fail_to_load_page,
-        toast = toast
-    ) {
-        HomeImageList(
-            listType = listType,
-            images = images,
-            listState = listState,
-            contentPadding = PaddingValues(
-                start = 8.dp, end = 8.dp, top = 16.dp
-            ),
-            spaceBy = 8f,
-            toDetail = toDetail,
-            loadImage = loadImage
-        )
-    }
-}
 
 /**
  * 실제 이미지 정보가 보이는 리스트
  */
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun HomeImageList(
-    listType: ListType,
+fun HomeImagePaging(
+    modifier: Modifier = Modifier,
     images: LazyPagingItems<ImageUiModel>,
-    listState: LazyListState = LazyListState(),
-    contentPadding: PaddingValues = PaddingValues(),
-    spaceBy: Float = 0f,
+    listState: LazyListState = rememberLazyListState(),
+    listType: ListType,
     toDetail: (String) -> Unit,
     loadImage: suspend (String, Int, Int) -> Bitmap?
 ) {
     LazyVerticalGrid(
+        modifier = modifier,
         cells = GridCells.Fixed(listType.column),
         state = listState,
-        contentPadding = contentPadding,
-        verticalArrangement = Arrangement.spacedBy(spaceBy.dp),
-        horizontalArrangement = Arrangement.spacedBy(spaceBy.dp)
+        contentPadding = PaddingValues(
+            start = 8.dp, end = 8.dp, top = 16.dp
+        ),
+        verticalArrangement = Arrangement.spacedBy(8f.dp),
+        horizontalArrangement = Arrangement.spacedBy(8f.dp)
     ) {
         item(span = { GridItemSpan(listType.column) }) { HomeImagePagingTitle() }
         items(images.itemCount) { index ->
