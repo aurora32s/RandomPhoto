0e462b1182c052f8dcd7f218df2414b5a2f3d7b5
aurora32s <aurora32s@naver.com>
2023-02-26 14:54:58 +0900

    [FEAT] #12 - Collapsing Toolbar 기본 UI 코드 추가

--- core/ui/src/main/java/com/haman/core/ui/list/PagingList.kt
+++ core/ui/src/main/java/com/haman/core/ui/list/PagingList.kt
@@ -1,10 +1,12 @@
 package com.haman.core.ui.list
 
+import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import com.haman.core.model.ui.UiModel
+import com.haman.core.ui.item.ErrorListItem
 
 /**
  * 페이징 데이터 처리 공통 Component
@@ -12,6 +14,8 @@ import com.haman.core.model.ui.UiModel
 @Composable
 fun <T : UiModel> PagingList(
     data: LazyPagingItems<T>,
+    @StringRes
+    errorMsg: Int,
     content: @Composable () -> Unit
 ) {
     when (data.loadState.refresh) {
@@ -19,11 +23,13 @@ fun <T : UiModel> PagingList(
         is LoadState.Error -> {
             // 초기 데이터 요청 실패
             if (data.itemCount == 0) {
-                // TODO 에러 아이템 출력
+                // 에러 아이템 출력
+                ErrorListItem(message = errorMsg)
             } else { // 페이징 데이터 실패
                 // TODO 에러 Toast 출력
             }
         }
+//        else -> content()
         else -> content()
     }
 }
--- feature/home/src/main/java/com/haman/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/com/haman/feature/home/HomeScreen.kt
@@ -1,22 +1,42 @@
 package com.haman.feature.home
 
+import androidx.compose.animation.core.FloatDecayAnimationSpec
+import androidx.compose.animation.core.FloatExponentialDecaySpec
+import androidx.compose.animation.core.animateDecay
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.mapSaver
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Velocity
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
-import com.haman.core.designsystem.component.AsyncImage
 import com.haman.core.designsystem.icon.DaangnIcons
 import com.haman.core.ui.item.ImageLinearItem
 import com.haman.core.ui.list.GridPagingList
-import com.haman.core.ui.list.PagingList
+import com.haman.core.designsystem.R
+import com.haman.core.designsystem.component.*
+import com.haman.core.designsystem.util.ImageType
+import kotlinx.coroutines.cancelChildren
+import kotlinx.coroutines.launch
+
+private val minHeightToolbar = 54.dp
+private val maxHeightToolbar = 340.dp
 
 /**
  * Home(Main) 화면
@@ -29,7 +49,6 @@ fun HomeScreen(
 ) {
     val images = viewModel.imagesInfo.collectAsLazyPagingItems()
     val listType = viewModel.listType.collectAsState()
-    val listState = rememberLazyListState()
 
     Scaffold(
         floatingActionButton = {
@@ -41,9 +60,13 @@ fun HomeScreen(
                 Icon(
                     modifier = Modifier.size(20.dp),
                     painter = painterResource(
-                        id = when (listType.value) {
-                            ListType.LINEAR -> DaangnIcons.gridBtn
-                            ListType.GRID -> DaangnIcons.linearBtn
+                        id = if (images.loadState.refresh is LoadState.Error) {
+                            DaangnIcons.refresh
+                        } else {
+                            when (listType.value) {
+                                ListType.LINEAR -> DaangnIcons.gridBtn
+                                ListType.GRID -> DaangnIcons.linearBtn
+                            }
                         }
                     ),
                     contentDescription = ""
@@ -51,10 +74,52 @@ fun HomeScreen(
             }
         }
     ) {
-        PagingList(data = images) {
+        val toolbarHeightRange = with(LocalDensity.current) {
+            minHeightToolbar.roundToPx()..maxHeightToolbar.roundToPx()
+        }
+        // toolbar 상태 정보
+        val toolbarState = rememberToolbarState(toolbarHeightRange = toolbarHeightRange)
+        val listState = rememberLazyListState()
+
+        val nestedScrollConnection = remember {
+            object : NestedScrollConnection {
+                /**
+                 * Scroll 이 발생하였을 떄
+                 */
+                override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
+                    toolbarState.scrollTopLimitReached =
+                        listState.firstVisibleItemIndex == 0 && listState.firstVisibleItemScrollOffset == 0
+                    toolbarState.scrollOffset = toolbarState.scrollOffset - available.y
+                    println("${available.y}")
+                    return Offset(0f, toolbarState.consumed)
+                }
+            }
+        }
+
+        Box(modifier = Modifier.nestedScroll(nestedScrollConnection)) {
+            CollapsingToolbar(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(
+                        with(LocalDensity.current) { toolbarState.height.toDp() }
+                    ),
+                imageType = ImageType.DrawableImage(R.drawable.profile_sample),
+                state = toolbarState.progress
+            )
             GridPagingList(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .graphicsLayer {
+                        translationY = toolbarState.height
+                    },
                 cell = listType.value.column,
-                contentPadding = 8f,
+                contentPadding = PaddingValues(
+                    bottom = minHeightToolbar,
+                    start = 8.dp,
+                    end = 8.dp,
+                    top = 16.dp
+                ),
+                space = 8f,
                 data = images,
                 listState = listState
             ) {
@@ -77,5 +142,12 @@ fun HomeScreen(
                 }
             }
         }
+    }
+}
+
+@Composable
+private fun rememberToolbarState(toolbarHeightRange: IntRange): ToolbarState {
+    return rememberSaveable(saver = ExitUnitCollapsedState.Saver) {
+        ExitUnitCollapsedState(toolbarHeightRange)
     }
 }
