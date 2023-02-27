7fe916d163fd4a2edbec2d606aabd0572f9bba6d
aurora32s <aurora32s@naver.com>
2023-02-27 19:38:37 +0900

    [REFACTOR] #44 - feature.home 코드 정리 및 상세화면 갔다가 돌아왔을 때 리스트 위치 초기화 되는 문제 해결

--- feature/home/src/main/java/com/haman/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/com/haman/feature/home/HomeScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.graphicsLayer
@@ -30,26 +31,34 @@ import com.haman.feature.home.ui.HomeFloatingButton
 import com.haman.feature.home.ui.HomeImagePaging
 import com.haman.feature.home.ui.HomeToolbar
 
+/**
+ * Toolbar 높이 범위
+ */
 private val minHeightToolbar = 54.dp
 private val maxHeightToolbar = 340.dp
 
 /**
  * Home(Main) 화면
  * @param toDetail 이미지 클릭 시, 상세 화면으로 이동
+ * @param toast Toast 띄우기 Event
+ * @param completeLoadInitData 초기 데이터 요청 성공 시의 Event
  */
 @Composable
 fun HomeScreen(
     toDetail: (String, String) -> Unit,
     toast: (ToastPosition, Int) -> Unit,
     completeLoadInitData: () -> Unit,
-    viewModel: HomeViewModel = hiltViewModel()
+    viewModel: HomeViewModel = hiltViewModel(),
 ) {
     val images = viewModel.imagesInfo.collectAsLazyPagingItems()
+
     LaunchedEffect(key1 = images.loadState.refresh) {
+        // 최초의 데이터 요청
         if (images.loadState.refresh != LoadState.Loading) {
             completeLoadInitData()
         }
     }
+
     // Toolbar 가능 높이
     val toolbarHeightRange = with(LocalDensity.current) {
         minHeightToolbar.roundToPx()..maxHeightToolbar.roundToPx()
@@ -57,7 +66,7 @@ fun HomeScreen(
 
     val toolbarState = rememberToolbarState(toolbarHeightRange = toolbarHeightRange)
     val listState = rememberLazyListState()
-    val listType = remember { mutableStateOf(ListType.GRID) }
+    val listType = rememberSaveable { mutableStateOf(ListType.GRID) }
 
     val nestedScrollConnection = remember {
         object : NestedScrollConnection {
@@ -66,11 +75,11 @@ fun HomeScreen(
                 toolbarState.scrollTopLimitReached =
                     listState.firstVisibleItemIndex == 0 && listState.firstVisibleItemScrollOffset == 0
                 toolbarState.scrollOffset = toolbarState.scrollOffset - available.y
-
                 return Offset(0f, toolbarState.consumed)
             }
         }
     }
+
     Scaffold(
         floatingActionButton = {
             HomeFloatingButton(
--- feature/home/src/main/java/com/haman/feature/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/haman/feature/home/HomeViewModel.kt
@@ -1,7 +1,5 @@
 package com.haman.feature.home
 
-import android.app.Application
-import android.content.pm.ApplicationInfo
 import android.graphics.Bitmap
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -10,29 +8,25 @@ import androidx.paging.cachedIn
 import androidx.paging.map
 import com.haman.core.domain.GetImageUseCase
 import com.haman.core.domain.GetImagesInfoUseCase
-import com.haman.core.domain.GetRandomImageInfoUseCase
 import com.haman.core.model.ui.ImageUiModel
 import com.haman.core.model.ui.toUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     getImagesInfoUseCase: GetImagesInfoUseCase,
-    private val getImageUseCase: GetImageUseCase,
-    private val getRandomImageInfoUseCase: GetRandomImageInfoUseCase
+    private val getImageUseCase: GetImageUseCase
 ) : ViewModel() {
-
-    private var loadImageJob = SupervisorJob()
     val imagesInfo: Flow<PagingData<ImageUiModel>> =
         getImagesInfoUseCase()
             .map { it.map { image -> image.toUiModel() } }
             .cachedIn(viewModelScope)
 
     /**
-     * 이미지 id 를 이용해
+     * 이미지 id 를 이용해 이미지 Bitmap 요청
      */
     suspend fun getImageByUrl(id: String): Bitmap? {
         return getImageUseCase(id)
