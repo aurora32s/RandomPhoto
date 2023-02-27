640ba2ab3524e4f01ab4108a8f22a934cf291d90
aurora32s <aurora32s@naver.com>
2023-02-27 12:55:10 +0900

    [FEAT] #17 - DetailScreen에서 이미지 상세 정보 요청 실패 시 UI 처리

--- feature/detail/src/main/java/com/haman/feature/detail/navigation/DetailNavigation.kt
+++ feature/detail/src/main/java/com/haman/feature/detail/navigation/DetailNavigation.kt
@@ -1,7 +1,10 @@
 package com.haman.feature.detail.navigation
 
-import androidx.navigation.*
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.haman.core.designsystem.theme.DaangnBlackTheme
 import com.haman.feature.detail.DetailScreen
 
@@ -19,7 +22,9 @@ fun NavController.navigateToDetail(imageId: String) {
  * DetailScreen
  * @param onBackPressed 이전 화면으로 이동
  */
-fun NavGraphBuilder.detailScreen(onBackPressed: () -> Unit) {
+fun NavGraphBuilder.detailScreen(
+    onBackPressed: () -> Unit
+) {
     composable(
         route = DetailNavigationRoute,
         arguments = arguments
