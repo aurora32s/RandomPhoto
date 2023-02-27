8f2465162fb2052059e9661c3fa94ddb2a8783f0
aurora32s <aurora32s@naver.com>
2023-02-27 17:17:41 +0900

    [REFACTOR] #44 - core.ui 코드 정리 및 주석 추가

--- core/ui/src/main/java/com/haman/core/ui/item/ImageLinearItem.kt
+++ core/ui/src/main/java/com/haman/core/ui/item/ImageLinearItem.kt
@@ -1,7 +1,10 @@
 package com.haman.core.ui.item
 
 import android.graphics.Bitmap
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
