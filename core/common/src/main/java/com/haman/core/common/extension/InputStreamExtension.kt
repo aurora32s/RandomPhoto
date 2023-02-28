0d76ded775aafb63f2728b4154e40c278e1f467e
aurora32s <aurora32s@naver.com>
2023-03-01 01:46:53 +0900

    [REFACTOR] #48 - 이미지 리스트와 함께 이미지를 요청하는 로직 제거

--- core/common/src/main/java/com/haman/core/common/extension/InputStreamExtension.kt
+++ core/common/src/main/java/com/haman/core/common/extension/InputStreamExtension.kt
@@ -1,10 +1,23 @@
 package com.haman.core.common.extension
 
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import android.graphics.BitmapFactory.Options
 import java.io.InputStream
 
-suspend fun InputStream?.decodeImage() = withContext(Dispatchers.Default) {
+fun InputStream?.decodeImage(): Bitmap? =
     BitmapFactory.decodeStream(this@decodeImage)
+
+private fun calculateSamplingSize(options: Options, width: Int, height: Int): Int {
+    val (imageHeight, imageWidth) = options.run { outHeight to outWidth }
+    var inSampleSize = 1
+
+    if (imageHeight > height || imageWidth > width) {
+        val halfHeight = imageHeight / 2
+        val halfWidth = imageWidth / 2
+        while (halfHeight / inSampleSize >= height && halfWidth / inSampleSize >= width) {
+            inSampleSize++
+        }
+    }
+    return inSampleSize
 }
