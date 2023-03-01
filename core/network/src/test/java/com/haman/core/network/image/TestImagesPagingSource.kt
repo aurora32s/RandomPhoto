64edc7f2eca98d9a7acf90e53f74d149519a220d
aurora32s <aurora32s@naver.com>
2023-03-01 17:35:58 +0900

    [TEST] #47 - ImagesPagingSource.load 메서드 성공 사례 테스트 코드 작성

--- core/network/src/test/java/com/haman/core/network/image/TestImagesPagingSource.kt
+++ core/network/src/test/java/com/haman/core/network/image/TestImagesPagingSource.kt
@@ -2,18 +2,38 @@ package com.haman.core.network.image
 
 import androidx.paging.PagingSource
 import com.haman.core.model.response.ImageResponse
+import com.haman.core.network.response.MockResponseFileReader
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
+import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.After
 import org.junit.Before
+import org.junit.Test
 import retrofit2.Retrofit
+import java.net.HttpURLConnection
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class TestImagesPagingSource {
 
     private lateinit var mockWebServer: MockWebServer
     private lateinit var imagePagingSource: PagingSource<Int, ImageResponse>
+
+    private val firstResponse = (1..2).map {
+        ImageResponse(
+            id = "$it",
+            author = "seomseom",
+            width = it * 1000,
+            height = it * 1000,
+            imageUrl = "image_$it"
+        )
+    }
 
     @Before
     fun setup() {
@@ -26,7 +46,7 @@ class TestImagesPagingSource {
                 Json {
                     coerceInputValues = true
                     ignoreUnknownKeys = true
-                }.asConverterFactory()
+                }.asConverterFactory("application/json".toMediaType())
             )
             .client(OkHttpClient())
             .build()
@@ -38,5 +58,35 @@ class TestImagesPagingSource {
     @After
     fun clear() {
         mockWebServer.shutdown()
+    }
+
+    @Test
+    fun pagingSource_load_Success() = runTest {
+        // 1. Given
+        val response = MockResponse()
+            .setResponseCode(HttpURLConnection.HTTP_OK)
+            .setBody(MockResponseFileReader("images_paging_success.json").content)
+        mockWebServer.enqueue(response)
+
+        // 2. When
+        val loadResult = imagePagingSource.load(
+            PagingSource.LoadParams.Refresh(
+                key = null,
+                loadSize = 2,
+                placeholdersEnabled = false
+            )
+        )
+
+        // 3. Then
+        assertThat(
+            loadResult,
+            `is`(
+                PagingSource.LoadResult.Page(
+                    data = firstResponse,
+                    prevKey = null,
+                    nextKey = 2
+                )
+            )
+        )
     }
 }
