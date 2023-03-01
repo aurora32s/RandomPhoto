bdfbccfe4e89c273f3e3f3187c8cf35e0d910a1d
aurora32s <aurora32s@naver.com>
2023-03-01 16:03:04 +0900

    [TEST] #47 - okhttp mockwebserver 및 ImageDataSourceImpl 초기 설정

--- core/network/src/test/java/com/haman/core/network/image/TestImageDataSourceImpl.kt
+++ core/network/src/test/java/com/haman/core/network/image/TestImageDataSourceImpl.kt
@@ -1,7 +1,42 @@
 package com.haman.core.network.image
 
+import com.haman.core.network.source.ImageDataSource
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
+import org.junit.Before
+import retrofit2.Retrofit
+
 /**
  * ImageDataSourceImpl 테스트 코드
  */
 class TestImageDataSourceImpl {
+
+    private lateinit var mockWebServer: MockWebServer
+    private lateinit var imageDataSource: ImageDataSource
+
+    @Before
+    fun setup() {
+        mockWebServer = MockWebServer()
+        mockWebServer.start()
+
+        val imageApiService = Retrofit.Builder()
+            .baseUrl(mockWebServer.url("/"))
+            .addConverterFactory(
+                Json {}.asConverterFactory("application/json".toMediaType())
+            )
+            .client(OkHttpClient())
+            .build()
+            .create(ImageApiService::class.java)
+
+        imageDataSource = ImageDataSourceImpl(imageApiService)
+    }
+
+    @After
+    fun clear() {
+        mockWebServer.shutdown()
+    }
 }
