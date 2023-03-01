218d98f1d37d0b69417c9dd578148ac27b072eae
aurora32s <aurora32s@naver.com>
2023-03-01 21:16:50 +0900

    [TEST] #46 - Disk Caching 테스트 및 imageCachedInDiskDataSource.addImage에 잘못 넘기던 paramter 수정

--- core/data/src/androidTest/java/com/haman/core/data/image/TestImageRepositoryImpl.kt
+++ core/data/src/androidTest/java/com/haman/core/data/image/TestImageRepositoryImpl.kt
@@ -7,18 +7,18 @@ import com.haman.core.data.fake.FakeImageDataSource
 import com.haman.core.data.repository.ImageRepository
 import com.haman.core.datastore.source.ImageCacheDataSource
 import com.haman.core.network.source.ImageDataSource
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.*
+import kotlinx.coroutines.test.*
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.notNullValue
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.lessThan
-import org.hamcrest.core.IsNot
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @RunWith(AndroidJUnit4::class)
 class TestImageRepositoryImpl {
 
@@ -27,20 +27,30 @@ class TestImageRepositoryImpl {
     private lateinit var imageDataSource: ImageDataSource
     private lateinit var imageRepository: ImageRepository
 
+    private val superJob = SupervisorJob()
+    private val externalScope = TestScope(superJob)
+    private val dispatcher = StandardTestDispatcher(externalScope.testScheduler)
+
     @Before
     fun setup() {
         imageCachedInMemoryDataSource = FakeImageCacheDataSource()
         imageCachedInDiskDataSource = FakeImageCacheDataSource()
         imageDataSource = FakeImageDataSource()
+        Dispatchers.setMain(dispatcher)
 
         imageRepository = ImageRepositoryImpl(
-            externalScope = CoroutineScope(Dispatchers.IO),
-            ioDispatcher = Dispatchers.IO,
+            externalScope,
+            dispatcher,
             imageCachedInMemoryDataSource,
             imageCachedInDiskDataSource,
             FakeImageApiService(),
             imageDataSource
         )
+    }
+
+    @After
+    fun clear() {
+        Dispatchers.resetMain()
     }
 
     @Test
@@ -60,6 +70,19 @@ class TestImageRepositoryImpl {
             assertThat(it.width, `is`(lessThan(100)))
             assertThat(it.height, `is`(lessThan(200)))
         }
+    }
+
+    @Test
+    fun getImage_Success_from_Disk() = runTest {
+        // 1. Given: 서버에서 데이터 요청
+        val reqWidth = 10
+        imageRepository.getImage(id = "0", width = 100, height = 200, reqWidth).getOrNull()
+
+        // 2. Disk 에 캐싱된 정보
+        val bitmap = imageCachedInDiskDataSource.getImage("0", reqWidth)
+
+        // 3. Then
+        assertThat(bitmap, `is`(notNullValue()))
     }
 
 }
