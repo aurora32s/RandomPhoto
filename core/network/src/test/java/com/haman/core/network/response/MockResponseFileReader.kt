6922a22ca6710967de617119aeaf61a971af3466
aurora32s <aurora32s@naver.com>
2023-03-01 16:34:35 +0900

    [TEST] #47 - 특정 이미지 정보 요청 성공 테스크 코드 작성

---
+++ core/network/src/test/java/com/haman/core/network/response/MockResponseFileReader.kt
@@ -0,0 +1,12 @@
+package com.haman.core.network.response
+
+import java.io.InputStreamReader
+
+class MockResponseFileReader(path: String) {
+    val content: String
+    init {
+        val reader = InputStreamReader(this.javaClass.classLoader?.getResourceAsStream(path))
+        content = reader.readText()
+        reader.close()
+    }
+}
