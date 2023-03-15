e521832e9b582da3558f8611a8e3ed7dbb7f3564
aurora32s <aurora32s@naver.com>
2023-03-15 15:30:55 +0900

    [FEAT] #58 - Repository에서 사용하던 ImageEntity를 ImageData로 변경 및 ImageEntity는 Database에서 사용할 수 있도록 Room Entity로 설정

--- core/data/src/main/java/com/haman/core/data/repository/ImageRepository.kt
+++ core/data/src/main/java/com/haman/core/data/repository/ImageRepository.kt
@@ -2,7 +2,7 @@ package com.haman.core.data.repository
 
 import android.graphics.Bitmap
 import androidx.paging.PagingData
-import com.haman.core.model.entity.ImageEntity
+import com.haman.core.model.entity.ImageData
 import kotlinx.coroutines.flow.Flow
 
 interface ImageRepository {
@@ -20,17 +20,17 @@ interface ImageRepository {
     /**
      * 이미지 리스트 요청
      */
-    fun getImagesInfo(): Flow<PagingData<ImageEntity>>
+    fun getImagesInfo(): Flow<PagingData<ImageData>>
 
     /**
      * 특정 이미지 정보 요청
      * @param id 이미지 id
      */
-    suspend fun getImageInfo(id: String): Result<ImageEntity?>
+    suspend fun getImageInfo(id: String): Result<ImageData?>
 
     /**
      * 랜덤 이미지 정보 요청
      * @param seed 랜덤에 사용될 seed 정보
      */
-    suspend fun getRandomImageInfo(seed: String): Result<ImageEntity?>
+    suspend fun getRandomImageInfo(seed: String): Result<ImageData?>
 }
---
+++ core/model/src/main/java/com/haman/core/model/repository/ImageData.kt
@@ -0,0 +1,37 @@
+package com.haman.core.model.repository
+
+import com.haman.core.model.entity.ImageEntity
+import com.haman.core.model.response.ImageResponse
+
+/**
+ * data module 의 이미지 정보 Model
+ */
+data class ImageData(
+    val id: String, // 이미지 id
+    val author: String, // 이미지 창작자 명
+    val width: Int, // 이미지 가로 길이
+    val height: Int, // 이미지 세로 길이
+    val imageUrl: String // 이미지 URL
+)
+
+/**
+ * network module image -> data module image
+ */
+fun ImageResponse.toData() = ImageData(
+    id = id,
+    author = author,
+    width = width,
+    height = height,
+    imageUrl = imageUrl
+)
+
+/**
+ * database module image -> data module image
+ */
+fun ImageEntity.toData() = ImageData(
+    id = id,
+    author = author,
+    width = width,
+    height = height,
+    imageUrl = imageUrl
+)
